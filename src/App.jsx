2f7f2f962833046947e58b80aee4be737c8af660
bibek190 <bibek9818917036@gmail.com>
2024-03-13 13:25:56 +1100

    routes and layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,34 @@
-import styled from "styled-components";
+// import React from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Bookings from "./pages/Bookings";
+import Cabins from "./pages/Cabins";
+import NewUsers from "./pages/Users";
+import Account from "./pages/Account";
+import Login from "./pages/Login";
+import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
-
-const H1 = styled.h1`
-  font-size: 30px;
-  font-weight: 600;
-`;
-
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 2rem;
-  border: none;
-  border-radius: var(--border-radius-sm);
-  background-color: var(--color-brand-600);
-  color: var(--color-brand-50);
-  margin: 20px;
-  box-shadow: var() (--shadow-sm);
-`;
-
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.5rem;
-`;
-
-const StyledApp = styled.div`
-  background-color: orangered;
-  padding: 20px;
-`;
+import AppLayout from "./ui/AppLayout";
 
 const App = () => {
   return (
     <>
       <GlobalStyles />
-      <StyledApp>
-        <H1>The Hotel</H1>
-        Hello world
-        <Button onClick={() => alert("hello")}>Spin</Button>
-        <Button onClick={() => alert("hello")}>Me </Button>
-        <Button onClick={() => alert("hello")}>bro</Button>
-        <Input type="number" placeholder="Number of guest" />
-      </StyledApp>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="bookings" element={<Bookings />} />
+            <Route path="cabins" element={<Cabins />} />
+            <Route path="users" element={<NewUsers />} />
+            <Route path="account" element={<Account />} />
+          </Route>
+
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 };
